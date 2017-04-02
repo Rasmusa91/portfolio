358f45a1f4248c6eb1822f3fa4e34d366937bb9e
Rasmus <rasmusa.a-91@hotmail.com>
2017-04-02 14:18:52 +0200

    Updated about info and its dynamic css

--- views/about.tpl.php
+++ views/about.tpl.php
@@ -1,30 +1,25 @@
 <div class="about">
-    <div class="top-bar container-fluid">
-         <ul class="social list-inline">
-             <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-             <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-             <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-             <li><a href="#"><i class="fa fa-github-alt" aria-hidden="true"></i></a></li>
-             <li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
-         </ul><!--//social-->
-     </div><!--//top-bar-->
-
     <div class="container text-center">
-        <img class="profile-image img-circle" src="https://cloud.graphicleftovers.com/23228/739863/abstract-colorful-tree.-copy-space-.eps-8.jpg" alt="">
+        <img class="profile-image img-circle" src="<?= SERVER_PATH . 'img/profile.png'; ?>" alt="Rasmus Appelqvist profile picture">
         <h1 class="name">Rasmus Appelqvist</h1>
-        <div class="title">Programmer</div>
+        <div class="title">Software Developer</div>
         <div class="profile">
             <p>Profile info goes here. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet.</p>
         </div>
     </div>
 
-    <div class="contact-info">
-        <div class="container text-center">
-            <ul class="list-inline">
-                <li class="email"><i class="fa fa-envelope"></i><a href="mailto:someone@example.com">james.doe@website.com</a></li>
-                <li><i class="fa fa-phone"></i> <a href="tel: 0123456789">0123 456 7890</a></li>
-                <li class="website"><i class="fa fa-globe"></i><a href="#" target="_blank">portfoliosite.com</a></li>
-            </ul>
-        </div><!--//container-->
-    </div><!--//contact-info-->
+    <div class="social container-fluid text-center">
+         <ul class="social-list list-inline">
+             <li><a href="https://www.linkedin.com/in/rasmus-appelqvist/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+             <li><a href="https://github.com/rasmusa91/"><i class="fa fa-github-alt" aria-hidden="true"></i></a></li>
+         </ul>
+     </div>
+
+    <div class="contact container text-center">
+        <ul class="list-inline">
+            <li class="email"><i class="fa fa-envelope"></i><a href="mailto:rasmus.a-91@hotmail.com">rasmus.a-91@hotmail.com</a></li>
+            <li><i class="fa fa-phone"></i> <a href="tel: 0123456789">070-575 37 54</a></li>
+            <li class="website"><i class="fa fa-globe"></i><a href="#" target="_blank">http://rasmusappelqvist.se</a></li>
+        </ul>
+    </div>
 </div>
