58c1294a26b252cc5e143e9440e0b53e8c28e434
Rasmus <rasmusa.a-91@hotmail.com>
2017-04-02 11:01:30 +0200

    Updated about

--- views/about.tpl.php
+++ views/about.tpl.php
@@ -1,4 +1,14 @@
 <div class="about">
+    <div class="top-bar container-fluid">
+         <ul class="social list-inline">
+             <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+             <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+             <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+             <li><a href="#"><i class="fa fa-github-alt" aria-hidden="true"></i></a></li>
+             <li><a href="#"><i class="fa fa-skype" aria-hidden="true"></i></a></li>
+         </ul><!--//social-->
+     </div><!--//top-bar-->
+
     <div class="container text-center">
         <img class="profile-image img-circle" src="https://cloud.graphicleftovers.com/23228/739863/abstract-colorful-tree.-copy-space-.eps-8.jpg" alt="">
         <h1 class="name">Rasmus Appelqvist</h1>
@@ -7,4 +17,14 @@
             <p>Profile info goes here. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet.</p>
         </div>
     </div>
+
+    <div class="contact-info">
+        <div class="container text-center">
+            <ul class="list-inline">
+                <li class="email"><i class="fa fa-envelope"></i><a href="mailto:someone@example.com">james.doe@website.com</a></li>
+                <li><i class="fa fa-phone"></i> <a href="tel: 0123456789">0123 456 7890</a></li>
+                <li class="website"><i class="fa fa-globe"></i><a href="#" target="_blank">portfoliosite.com</a></li>
+            </ul>
+        </div><!--//container-->
+    </div><!--//contact-info-->
 </div>
